d5b7ed6e4629edbe0480f31c50bd345accde74b7
schiebout <91149931+schiebout@users.noreply.github.com>
2021-09-21 13:18:06 -0400

    Create geneweights.R
    Brief code to format gene weights from an input gene set properly for VAM usage

---
+++ geneweights.R
@@ -0,0 +1,13 @@
+df <- df[!is.na(df$ensembl_gene),]
+
+gene.w <- list()
+for (j in 1:length(gene.set.collection)){
+  gwi <- c()
+  for (i in 1:length(names(gene.set.collection[[j]]))){
+    gwi <- c(gwi, which(df$ensembl_gene==names(gene.set.collection[[j]][i])))
+  }
+  gwi <- intersect(gwi, which(df$V1 == names(gene.set.collection)[j]))
+  gene.w[[j]] <- (as.double(df[gwi,3]))
+}
+
+gene.w
